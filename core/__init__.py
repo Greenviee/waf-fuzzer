0b31f2f82e5c88589080ead52326da35029c14c4
Jinwoo Kim <131782814+Greenviee@users.noreply.github.com>
2026-04-29 14:50:10 +0900

    Revert "[fix] sqli 모듈 오탐 제거를 위한 모듈 전체 수정"

--- core/__init__.py
+++
@@ -1,9 +0,0 @@
-from .models import AttackSurface, FuzzingTask, HttpMethod, ParamLocation, Payload
-
-__all__ = [
-    "AttackSurface",
-    "HttpMethod",
-    "ParamLocation",
-    "Payload",
-    "FuzzingTask",
-]
